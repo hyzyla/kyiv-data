b384d6726833ec31feb2886ed6f11854bf35d40d
Yevhenii Hyzyla <hyzyla@gmail.com>
2020-11-15 13:59:17 +0200

    Add sentry

--- app/config.py
+++ app/config.py
@@ -1,9 +1,12 @@
+from typing import Optional
+
 from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
     DATABASE_URL: str
     CC_HOST: str = '1551-back.kyivcity.gov.ua'
+    SENTRY_DSN: Optional[str] = None
 
 
 settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 import logging
 
+import sentry_sdk
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_marshmallow import Marshmallow
 from flask_admin import Admin
+from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from sentry_sdk.integrations.flask import FlaskIntegration
 
 from app.config import settings
 
@@ -14,6 +16,12 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = settings.DATABASE_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+
+sentry_sdk.init(
+    dsn=settings.SENTRY_DSN,
+    integrations=[FlaskIntegration()],
+    traces_sample_rate=1.0
+)
 
 admin = Admin(app, name='Kyiv data', template_mode='bootstrap3')
 db = SQLAlchemy(app)
