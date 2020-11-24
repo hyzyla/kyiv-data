685f7b0cd7bac7a36cfc019d4ffa19c44b4a9487
Yevhenii Hyzyla <hyzyla@gmail.com>
2020-11-24 22:15:07 +0200

    Add logging

--- app/lib/validators.py
+++ app/lib/validators.py
@@ -5,6 +5,7 @@ from marshmallow import ValidationError, Schema
 
 from app.lib.errors import SchemaValidatorError
 from app.lib.types import DataDict
+from app.main import app
 
 T = TypeVar('T', bound=Schema)
 
@@ -12,6 +13,8 @@ T = TypeVar('T', bound=Schema)
 def validate_request_json(schema: T) -> DataDict:
     """ Validate request data by schema """
     data = request.get_json()
+    app.logger.info(f'JSON data {type(data)}: {data}')
+    print(f'JSON data {type(data)}: {data}')
     try:
         return schema.load(data)
     except ValidationError as err:
