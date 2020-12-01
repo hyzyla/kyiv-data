9b89388ea8b3d17ae5b07d8733ee342a18d1f4f6
Yevhenii Hyzyla <hyzyla@gmail.com>
2020-12-01 16:03:47 +0200

    Fix admin view

--- app/extensions.py
+++ app/extensions.py
@@ -4,7 +4,7 @@ from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-admin = Admin(name='Kyiv data', template_mode='bootstrap3', endpoint='/admin')
+admin = Admin(name='Kyiv data', template_mode='bootstrap3', endpoint='admin', url='/admin')
 db = SQLAlchemy()
 ma = Marshmallow()
 migrate = Migrate()
